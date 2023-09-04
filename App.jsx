f400c160ddbb13e43e415981c0ed4467095ae587
anaangelieri <131811398+anaangelieri@users.noreply.github.com>
2023-09-04 20:15:03 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Booklist from './componentes/Book'
+
+function App() {
+
+  return (
+    <>
+      <section>
+      <h1>Lista de livros</h1>
+        <Booklist />
+      </section>
+    </>
+  )
+}
+
+export default App
